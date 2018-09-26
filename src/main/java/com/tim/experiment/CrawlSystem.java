1428242bac733f53de181c72f28b0e117f60f506
Timothy Farkas <tim@streamsimple.io>
2018-09-26 14:43:18 -0700

    - Plugged in the crawler to the system.

--- src/main/java/com/tim/experiment/CrawlSystem.java
+++ src/main/java/com/tim/experiment/CrawlSystem.java
@@ -2,6 +2,7 @@ package com.tim.experiment;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Queue;
@@ -9,16 +10,23 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class CrawlSystem
 {
-  private PageLoader pageLoader = new PageLoader();
+  private int numCrawlers = 4;
+  private final PageLoader pageLoader = new PageLoader();
+  private final CrawlFetcher fetcher;
+  private final CrawlParser parser;
 
+  private List<Crawl> crawlers = new ArrayList<>();
   private Queue<String> urlQueue = new ConcurrentLinkedQueue<>();
   private Map<String, String> firstDegreeTable = new ConcurrentHashMap<>();
-  private ExecutorService workerPool = Executors.newFixedThreadPool(4);
+  private ExecutorService workerPool = Executors.newFixedThreadPool(numCrawlers);
 
-  public CrawlSystem() {
+  public CrawlSystem(final CrawlFetcher fetcher, final CrawlParser parser) {
+    this.fetcher = fetcher;
+    this.parser = parser;
   }
 
   public void initialize(File seed) throws FileNotFoundException
@@ -27,10 +35,24 @@ public class CrawlSystem
 
     urlQueue.addAll(urls);
 
-    //workerPool.submit()
+    for (int counter = 0; counter < numCrawlers; counter++) {
+      final Crawl crawl = new Crawl(urlQueue, firstDegreeTable, fetcher, parser);
+      crawlers.add(crawl);
+      workerPool.submit(crawl);
+    }
   }
 
-  public List<List<String>> getShortestConnections() {
+  /**
+   * @throws InterruptedException If not able to shutdown cleanly.
+   */
+  public void terminate() throws InterruptedException
+  {
+    crawlers.forEach(crawl -> crawl.terminate());
+    workerPool.shutdown();
+    workerPool.awaitTermination(1, TimeUnit.MINUTES);
+  }
+
+  public List<List<String>> getShortestConnections(String page1, String page2) {
     return null;
   }
 }
